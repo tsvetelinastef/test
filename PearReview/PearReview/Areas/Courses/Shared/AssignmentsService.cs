2be0b260dbaa1225b7fb831f4ca667bdfae2be8e
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-05-01 14:30:30 +0300

    modify

---
+++ PearReview/PearReview/Areas/Courses/Shared/AssignmentsService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PearReview.Areas.Courses.Pages;
+using PearReview.Data;
+using PearReview.Models;
+
+namespace PearReview.Services
+{
+    public class AssignmentsService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public AssignmentsService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Assignment>> GetAssignmentsForCourseAsync(int courseId)
+        {
+            return await _dbContext.Assignments.Where(a => a.CourseId == courseId).ToListAsync();
+        }
+    }
+}
