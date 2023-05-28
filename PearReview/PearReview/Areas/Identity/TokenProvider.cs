827c976b17b90d1da1e387a0734e0e3c80172e15
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-05-28 12:54:03 +0300

    Delete PearReview directory

--- PearReview/PearReview/Areas/Identity/TokenProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PearReview.Areas.Identity
-{
-    public class TokenProvider
-    {
-        public string XsrfToken { get; set; } = string.Empty;
-
-        public string RefreshToken { get; set; } = string.Empty;
-    }
-}
